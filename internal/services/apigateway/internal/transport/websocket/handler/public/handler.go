daa7e03e1ba7d3706b680f254ed3c7566a6c5b7d
Артём Кутузов <tema.kutuzzzov@yandex.ru>
2025-01-30 22:42:29 +0300

    Test #2

--- internal/services/apigateway/internal/transport/websocket/handler/public/handler.go
+++ internal/services/apigateway/internal/transport/websocket/handler/public/handler.go
@@ -23,6 +23,7 @@ func (h *Handler) Handle(ws *websocket.Conn) {
 
 	for {
 		// TODO chans for receive and send
+		// Test2
 		if err := websocket.Message.Send(ws, "Hello))"); err != nil {
 			handleErr = fmt.Errorf("error send message. %w", err)
 
